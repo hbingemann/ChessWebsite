e1abb208daf475833d41004ebee5cf97e67ccfa0
henrik <henrik.bingemann@gmail.com>
2022-01-09 16:44:36 -0500

    updated api to be in pycharm with a venv

---
+++ python-api/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from stockfish import Stockfish
+import json
+import requests
+import chessdotcom
+
+
+app = Flask(__name__)
+
+stockfish_location = "/stockfish\stockfish_14.1_win_x64_avx2\stockfish_14.1_win_x64_avx2.exe"
+stockfish = Stockfish(stockfish_location, depth=15)
+
+
+@app.route("/getpgn", methods=["POST"])
+def get_pgn():
+    posted_info = request.json
+    platform = posted_info["from"]
+    user = posted_info["user"]
+
+    pgn = None
+
+    if platform == "chessdotcom":
+        months = chessdotcom.get_player_game_archives(user).archives
+        recent_games = requests.get(months[-1]).json()
+        recent_games = recent_games["games"]
+        last_game = recent_games[-1]
+        pgn = last_game["pgn"]
+
+    return json.dumps({
+        "pgn": pgn
+    })
+
+
+@app.route("/topmoves", methods=["POST"])
+def top_moves():
+    posted_info = request.json
+    position = posted_info["position"]
+    moves = int(posted_info["moves"])
+
+    stockfish.set_fen_position(position)
+    move_choices = stockfish.get_top_moves(moves)
+
+    return json.dumps(move_choices)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
